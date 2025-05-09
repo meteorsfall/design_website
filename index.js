170a2629b0a9eed1c1fdec2e85240d3c66b53029
Louise Song <louise.s.song@gmail.com>
2025-05-08 17:14:43 -0700

    finished messages js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const messages = document.getElementById("messages")
+const messages = document.getElementById("messages");
 messages.style.display = "none";
+const message = document.getElementById("message");
+let copy1 = message.cloneNode(true);
+let copy2 = message.cloneNode(true);
+copy2.style.borderBottom = "0px";
+messages.appendChild(copy1);
+messages.appendChild(copy2);
 
 function showMessages() {
     const messages = document.getElementById("messages");
