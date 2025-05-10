99d9184800c324372014731c5bdd0562daedf9ad
Louise Song <louise.s.song@gmail.com>
2025-05-09 17:20:33 -0700

    finished javascript

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ copy2.style.borderBottom = "0px";
 messages.appendChild(copy1);
 messages.appendChild(copy2);
 
-const dropdown = document.getElementById("dropdown");
-const dropdownMenu = document.getElementById("dropdown-menu");
-dropdownMenu.style.display = "none";
-dropdown.onclick = () => dropdownMenu.style.display = dropdownMenu.style.display == "none"? "flex" : "none";
-
 function showMessages() {
     const messages = document.getElementById("messages");
     messages.style.display = messages.style.display === "none" ? "flex" : "none";
@@ -38,5 +33,39 @@ document.addEventListener("click", function(event) {
 const dropdownLines = document.getElementsByClassName("dropdown-line");
 for (let i = 0; i < dropdownLines.length; i++){
     let dropdownLine = dropdownLines[i];
-    dropdownLine.onclick = () => dropdownLine.parentNode.parentNode.innerText = dropdownLine.innerText;
+    dropdownLine.onclick = () => dropdownLine.parentNode.parentNode.querySelector("span").innerHTML = dropdownLine.innerText;
+}
+
+const turnDarkElems = document.getElementsByClassName("turn-dark");
+for (let i = 0; i < turnDarkElems.length; i++){
+    let turnDarkElem = turnDarkElems[i];
+    turnDarkElem.addEventListener("click", function() {
+        if (turnDarkElem.style.backgroundColor == "rgb(157, 205, 205)"){
+            turnDarkElem.style.backgroundColor = "rgb(44, 61, 81)";
+            turnDarkElem.style.color = "rgb(157, 205, 205)";
+        } else {
+            turnDarkElem.style.backgroundColor = "rgb(157, 205, 205)";
+            turnDarkElem.style.color = "black";
+        }
+    });
+}
+
+const watchElems = document.getElementsByClassName("watch");
+for (let i = 0; i < watchElems.length; i++){
+    let watchElem = watchElems[i];
+    watchElem.addEventListener("click", function() {
+        if (watchElem.textContent == "Watch"){
+            watchElem.textContent = "Watching";
+        } else {
+            watchElem.textContent = "Watch";
+        }
+    });
+}
+
+const icon2Elems = document.getElementsByClassName("icon2");
+for (let i = 0; i < icon2Elems.length; i++){
+    let icon2Elem = icon2Elems[i];
+    icon2Elem.addEventListener("click", function() {
+        this.classList.toggle("active");
+    });
 }
